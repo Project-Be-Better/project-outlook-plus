e32d747448bca538e201f840f54f865a2b7a3ecd
sree <sreeraj_ec@hotmail.com>
2025-08-21 14:14:19 +0800

    20250821-Succesfully compiled electroncd

--- dist-electron/preload.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const electron_1 = require("electron");
-electron_1.contextBridge.exposeInMainWorld("electronAPI", {
-    sendMessage: (msg) => electron_1.ipcRenderer.send("message", msg),
-    onMessage: (callback) => electron_1.ipcRenderer.on("message", (_, msg) => callback(msg)),
-});
