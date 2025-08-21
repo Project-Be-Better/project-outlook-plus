2f7135a418db505213bc567fb24d549e5c85956b
sree <sreeraj_ec@hotmail.com>
2025-08-21 14:17:45 +0800

    20250821-Folder restructured

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Outlook-
-This is an electron prototype to get me started with the development best practices 
+
+This is an electron prototype to get me started with the development best practices
+
+How to Run
+
+```bash
+npx tsc -p apps/electron && npx electron ./apps/dist-electron/main.js
+```
---
+++ apps/electron/main.ts
@@ -0,0 +1,54 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+import * as dotenv from "dotenv";
+
+// load config file
+// dotenv.config({ path: path.resolve(__dirname, "../.env") });
+dotenv.config({ path: path.resolve(__dirname, "../../.env") });
+let mainWindow: BrowserWindow | null = null;
+
+const createWindow = () => {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  // if (process.env.NODE_ENV === "development") {
+  //   mainWindow.loadURL("http://localhost:5173");
+  // } else {
+  //   mainWindow.loadFile(path.join(__dirname, "../frontend/dist/index.html"));
+  // }
+
+  if (process.env.NODE_ENV === "development") {
+    mainWindow.loadURL("http://localhost:5173");
+  } else {
+    mainWindow.loadFile(path.join(__dirname, "../frontend/dist/index.html"));
+  }
+
+  if (process.env.NODE_ENV === "development") {
+    mainWindow.webContents.openDevTools();
+  }
+
+  mainWindow.on("closed", () => {
+    mainWindow = null;
+  });
+};
+
+// Open dev tools automatically in dev mode
+app.whenReady().then(createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// macOS: Re-create a window when app is activated and no windows are open
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
